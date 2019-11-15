5dd8bbb1bfa8eb27e6a532523773b222b54090b1
ayrtonsbarreto <ayrtonsbarreto@outlook.com>
2019-11-15 10:46:18 -0300

    criada a validação dos campos da tela de criar disciplina

--- app/src/main/java/com/example/taskmanager/ui/subject/RegisterSubjectFragment.kt
+++ app/src/main/java/com/example/taskmanager/ui/subject/RegisterSubjectFragment.kt
@@ -1,6 +1,7 @@
 package com.example.taskmanager.ui.subject
 
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,12 +10,15 @@ import android.widget.Toast
 import com.example.taskmanager.R
 import com.example.taskmanager.business.SubjectBusiness
 import com.example.taskmanager.business.UserBusiness
+import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.fragment_register_subject.*
 
 
 class RegisterSubjectFragment : Fragment() {
 
     private lateinit var subjectBusiness: SubjectBusiness
+
+    var dadosValidados:Boolean = false
 
     override fun onResume() {
         super.onResume()
@@ -28,7 +32,14 @@ class RegisterSubjectFragment : Fragment() {
 
     private fun buttons() {
         btnRegister.setOnClickListener {
-           subjectBusiness.insert(txtName.text.toString(), txtDescription.text.toString())
+           dadosValidados = validarFormulario()
+           if (dadosValidados){
+               subjectBusiness.insert(txtName.text.toString(), txtDescription.text.toString())
+           }else{
+               Toast.makeText(this!!.context, getString(R.string.dados_incorretos), Toast.LENGTH_LONG)
+                   .show()
+           }
+
         }
 
     }
@@ -43,4 +54,25 @@ class RegisterSubjectFragment : Fragment() {
     }
 
 
+
+    private fun validarFormulario():Boolean{
+        //Regra de validação
+        var retorno = false
+
+        if (!TextUtils.isEmpty(txtName.text.toString())){
+            retorno = true
+        }else{
+            txtName.setError("Por favor, insira um nome para a disciplina")
+            txtName.requestFocus()
+        }
+
+        if (!TextUtils.isEmpty(txtDescription.text.toString())){
+            retorno = true
+        }else{
+            txtDescription.setError("Por favor, insira uma descrição")
+            txtDescription.requestFocus()
+        }
+
+        return retorno
+    }
 }
